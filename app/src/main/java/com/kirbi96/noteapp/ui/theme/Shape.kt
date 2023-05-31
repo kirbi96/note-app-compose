7a372350d9fc677f71ceb261a3d230e77c17a776
makarovka <makarovka@sovcombank.ru>
2023-05-31 09:48:15 +0300

    init

---
+++ app/src/main/java/com/kirbi96/noteapp/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.kirbi96.noteapp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
